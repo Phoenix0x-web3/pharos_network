217a46966a7ebba08b7999cc856aac3f3781b361
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-08-12 15:07:12 +0400

    Introduce the log level (#17)
    Merge settings sync
    
    Co-authored-by: Alex Chernyi <ote1188@gmail.com>

--- utils/create_files.py
+++ utils/create_files.py
@@ -3,7 +3,6 @@ from libs.eth_async.utils.files import touch
 import os
 from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedMap
-from ruamel.yaml.util import load_yaml_guess_indent
 from copy import deepcopy
 
 REQUIRED_FILES = [
@@ -19,49 +18,64 @@ def create_files() -> None:
         touch(path=os.path.join(FILES_DIR, name), file=True)
     create_yaml()
 
+def load_yaml_file(path: str) -> CommentedMap:
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.explicit_start = True
+    yaml.explicit_end = True
+    if not os.path.exists(path):
+        return CommentedMap()
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            loaded = yaml.load(f)
+            return loaded or CommentedMap()
+    except Exception:
+        return CommentedMap()
+
 def create_yaml():
     yaml = YAML()
     yaml.indent(mapping=2, sequence=4, offset=2)
     yaml.preserve_quotes = True
+    yaml.explicit_start = True
+    yaml.explicit_end = True
     template_settings = load_yaml_file(TEMPLATE_SETTINGS_FILE)
     current_settings = load_yaml_file(SETTINGS_FILE)
     updated_settings = merge_settings(current_settings, template_settings)
     with open(SETTINGS_FILE, "w", encoding="utf-8") as f:
         yaml.dump(updated_settings, f)
 
-def load_yaml_file(path: str) -> CommentedMap:
-    if not os.path.exists(path):
-        return CommentedMap()
-    with open(path, "r", encoding="utf-8") as f:
-        loaded, _, _ = load_yaml_guess_indent(f)
-        return loaded or CommentedMap()
-
 def merge_settings(current: CommentedMap, template: CommentedMap) -> CommentedMap:
-    for key, value in template.items():
+    result = CommentedMap()
+
+    if hasattr(template, 'ca') and getattr(template.ca, 'comment', None) and template.ca.comment:
+        comment_text = ''
+        for comment_item in template.ca.comment:
+            if isinstance(comment_item, list):
+                for sub_item in comment_item:
+                    if sub_item and hasattr(sub_item, 'value'):
+                        comment_text += sub_item.value
+            elif comment_item and hasattr(comment_item, 'value'):
+                comment_text += comment_item.value
+        if comment_text.strip():
+            result.yaml_set_start_comment(comment_text)
+
+    for key in template.keys():
         if key not in current:
-            current[key] = deepcopy(value)
-            if hasattr(template, 'ca') and key in template.ca.items:
-                if not hasattr(current, 'ca'):
-                    current.ca = type(template.ca)()
-                current.ca.items[key] = template.ca.items[key]
+            result[key] = deepcopy(template[key])
+        elif isinstance(template[key], dict) and isinstance(current[key], dict):
+            result[key] = merge_settings(current[key], template[key])
+        else:
+            result[key] = current[key]
 
-        elif isinstance(value, dict) and isinstance(current[key], dict):
-            current[key] = merge_settings(current[key], value)
+        if hasattr(template, 'ca') and key in template.ca.items and template.ca.items[key][2]:
+            result.ca.items[key] = deepcopy(template.ca.items[key])
 
-        elif hasattr(template, 'ca') and key in template.ca.items:
-            t_comment = template.ca.items[key]
-            c_comment = current.ca.items.get(key) if hasattr(current, 'ca') else None
-            if t_comment != c_comment:
-                if not hasattr(current, 'ca'):
-                    current.ca = type(template.ca)()
-                current.ca.items[key] = t_comment
+    for key in current.keys():
+        if key not in template:
+            result[key] = current[key]
+            if hasattr(current, 'ca') and key in current.ca.items:
+                result.ca.items[key] = deepcopy(current.ca.items[key])
 
-    if hasattr(template, 'ca') and getattr(template.ca, 'comment', None):
-        if not hasattr(current, 'ca'):
-            current.ca = type(template.ca)()
-        if not getattr(current.ca, 'comment', None):
-            current.ca.comment = template.ca.comment
-
-    return current
+    return result
 
 create_files()
