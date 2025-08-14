38a03cdc06a6e1780bb5ba818b9f97a22bd07cef
Alex Chernyi <ote1188@gmail.com>
2025-08-15 00:12:28 +0400

    Revert "add log level"
    This reverts commit cd4e5956e12775a99c314c5263084e5c28c538a6.

--- functions/activity.py
+++ functions/activity.py
@@ -1,6 +1,5 @@
 import asyncio
 import random
-import sys
 from datetime import datetime, timedelta
 from typing import List
 
@@ -20,14 +19,6 @@ from utils.db_update import update_next_action_time, update_expired
 _running_wallets: set[int] = set()
 _pending_tasks: set[asyncio.Task] = set()
 semaphore = asyncio.Semaphore(Settings().threads)
-
-settings = Settings()
-
-# Configure the logger based on the settings
-if settings.log_level not in ["DEBUG", "INFO", "WARNING", "ERROR"]:
-    raise ValueError(f"Invalid log level: {settings.log_level}. Must be one of: DEBUG, INFO, WARNING, ERROR")
-logger.remove()  # Remove the default logger
-logger.add(sys.stderr, level=settings.log_level)
 
 async def random_activity():
     #check_encrypt_param()
