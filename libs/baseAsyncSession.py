dea7afab00cbf91323c075edfa75115c66caa6ae
phoenix <phoenix0x.web3@gmail.com>
2025-10-19 01:45:16 +0400

    Minor: fix FINGERPRINT_DEFAULT

--- libs/baseAsyncSession.py
+++ libs/baseAsyncSession.py
@@ -2,10 +2,12 @@ from curl_cffi import requests
 
 FINGERPRINT_DEFAULT = {
     "impersonate": "chrome136",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
-    "sec-ch-ua-platform": "Windows",
-    "sec-ch-ua": 'Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99',
-    "accept-language": "en-US,en;q=0.9",
+    "headers": {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": "Windows",
+        "sec-ch-ua": 'Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99',
+        "accept-language": "en-US,en;q=0.9",
+    },
 }
 
 FINGERPRINT_MAC136 = {
