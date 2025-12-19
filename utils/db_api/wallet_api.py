1e6ee004ffacc2d98497c4911dd989c29b7c3e07
dgloo <shaixee28@gmail.com>
2025-12-19 15:48:59 +0300

    twt and faucet

--- utils/db_api/wallet_api.py
+++ utils/db_api/wallet_api.py
@@ -10,47 +10,39 @@ def get_wallets(sqlite_query: bool = False) -> list[Wallet]:
     return db.all(entities=Wallet)
 
 
-def get_wallet_by_id(id: int, sqlite_query: bool = False) -> Wallet | None:
-    return db.one(Wallet, Wallet.id == id)
+def get_wallet_by_private_key(private_key: str, sqlite_query: bool = False) -> Wallet | None:
+    if sqlite_query:
+        return db.execute("SELECT * FROM wallets WHERE private_key = ?", (private_key,), True)
+
+    return db.one(Wallet, Wallet.private_key == private_key)
 
 
-def get_wallet_by_email_data(email_data: str) -> Wallet | None:
-    return db.one(Wallet, Wallet.email_data == email_data)
+def get_wallet_by_address(address: str, sqlite_query: bool = False) -> Wallet | None:
+    if sqlite_query:
+        return db.execute("SELECT * FROM wallets WHERE address = ?", (address,), True)
+
+    return db.one(Wallet, Wallet.address == address)
 
 
-def save_bearer_token(id: int, bearer_token: str) -> bool:
-    wallet = db.one(Wallet, Wallet.id == id)
+def update_twitter_token(private_key: str, updated_token: str | None) -> bool:
+    """
+    Updates the Twitter token for a wallet with the given private_key.
+
+    Args:
+        private_key: The private key of the wallet to update
+        new_token: The new Twitter token to set
+
+    Returns:
+        bool: True if update was successful, False if wallet not found
+    """
+    if not updated_token:
+        return False
+
+    wallet = db.one(Wallet, Wallet.private_key == private_key)
     if not wallet:
         return False
-    wallet.bearer_token = bearer_token
-    db.commit()
-    return True
 
-
-def save_refresh_token(id: int, refresh_token: str) -> bool:
-    wallet = db.one(Wallet, Wallet.id == id)
-    if not wallet:
-        return False
-    wallet.refresh_token = refresh_token
-    db.commit()
-    return True
-
-
-def update_points_and_top(id: int, points: int, top: int) -> bool:
-    wallet = db.one(Wallet, Wallet.id == id)
-    if not wallet:
-        return False
-    wallet.points = points
-    wallet.top = top
-    db.commit()
-    return True
-
-
-def update_discord_connect(id: int) -> bool:
-    wallet = db.one(Wallet, Wallet.id == id)
-    if not wallet:
-        return False
-    wallet.discord_connected = True
+    wallet.twitter_token = updated_token
     db.commit()
     return True
 
